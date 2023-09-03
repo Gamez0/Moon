67f15e0c5007aa5902df0ce087fc7b4a1d453847
신도빈의 맥 <dobinshin@gmail.com>
2023-09-03 19:44:32 +0900

    u: moon 2d 로 분리

---
+++ app/3d/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <main>This is 3d home page</main>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
-import Image from 'next/image';
-import { useEffect, useState } from 'react';
-import Moon from './component/Moon';
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { MouseEventHandler } from 'react';
 
 export default function Home() {
+  const router = useRouter();
+  const setRoute = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    const { route } = e.currentTarget.dataset;
+    router.push(route ?? '');
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <Moon />
+    <main className="flex min-h-screen ">
+      <div className="flex gap-[20px]">
+        <button data-route="/moon2d" onClick={setRoute}>
+          Go to Moon2d
+        </button>
+        <button data-route="/3d" onClick={setRoute}>
+          Go to 3d
+        </button>
+      </div>
     </main>
   );
 }
